9659ef374911d82b406117c1463c1f7101b9058d
Eva Adler <eadler@cartodb.com>
2026-05-18 20:23:08 -0500

    Create annotate_traces.py

---
+++ annotate_traces.py
@@ -0,0 +1,5 @@
+cp ~/annotate_traces.py ~/merit-parking-phoenix-demo/
+cd ~/merit-parking-phoenix-demo
+git add annotate_traces.py
+git commit -m "Add annotation script with hallucination detection and accuracy scoring"
+git push origin main
